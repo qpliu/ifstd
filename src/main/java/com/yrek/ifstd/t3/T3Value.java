084d466f5485967aad240dc708f55aa54bcb00d6
Q.P.Liu <qpliu@yahoo.com>
2014-09-30 17:37:23 -0700

    Start on TADS-3 interpreter.

--- src/main/java/com/yrek/ifstd/t3/T3Value.java
+++ src/main/java/com/yrek/ifstd/t3/T3Value.java
@@ -8,12 +8,8 @@ abstract class T3Value implements Serializable {
     static final T3Value NIL = new T3Value() {
         private static final long serialVersionUID = 0L;
     };
-    static final T3Value INT0 = new T3Value() {
-        private static final long serialVersionUID = 0L;
-    };
-    static final T3Value INT1 = new T3Value() {
-        private static final long serialVersionUID = 0L;
-    };
+    static final T3Value INT0 = new T3ValueInt(0);
+    static final T3Value INT1 = new T3ValueInt(1);
     static final T3Value TRUE = new T3Value() {
         private static final long serialVersionUID = 0L;
     };
